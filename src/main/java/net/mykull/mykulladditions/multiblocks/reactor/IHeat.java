c83756172f2a56ef778100d595c8ff7e95fbaeac
Mykull <michaelguillory.mg@gmail.com>
2025-07-09 22:10:21 -0700

    Heat

--- src/main/java/net/mykull/mykulladditions/multiblocks/reactor/IHeat.java
+++ src/main/java/net/mykull/mykulladditions/multiblocks/reactor/IHeat.java
@@ -1,4 +1,22 @@
 package net.mykull.mykulladditions.multiblocks.reactor;
 
+
 public interface IHeat {
+     /// @return amount of heat
+    int getHeat();
+
+    /// @return maximum heat (like explosion amount or sum IDK)
+    int getMaxHeat();
+
+    /**
+     * @param heat amount of heat to be added
+     * @return the amount of heat added
+     */
+    int addHeat(int heat);
+
+    /**
+     * @param heat amount of heat wanting to be removed
+     * @return the amount of heat actually removed
+     */
+    int removeHeat(int heat);
 }
